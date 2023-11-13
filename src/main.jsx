62e949d90929fef378b07090b3afe0b245f9d200
Miguel Pedraza <mikeped469@gmail.com>
2023-11-12 18:15:09 -0600

    Intial commit setup with vite, tailwind and react-router-dom

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// React imports
+import ReactDOM from "react-dom/client";
+
+// Components
+import App from "./App";
+
+// Styles
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
